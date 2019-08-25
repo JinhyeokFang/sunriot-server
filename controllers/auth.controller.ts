f69bd4608a1438235c9c5b1339ada128f0595aa6
jinhyeokfang <jinhyeokfang@gmail.com>
2019-08-26 07:46:12 +0900

    remove locallogin

--- controllers/auth.controller.ts
+++ controllers/auth.controller.ts
@@ -5,23 +5,6 @@ import { decodeToken, encodeToken, isVaildToken } from '../utils/jwt';
 import user from '../models/user.model'
 
 class AuthController {
-    public login(req: Request, res: Response): void {
-        let { username, password } = req.body;
-
-        user.login(username, password).then(result => {
-            ResponseSuccess(res, {token: encodeToken({
-                username: username,
-                time: new Date().getTime()
-            })});
-        }).catch(result => {
-            if (result.err == "the user not found") {
-                ResponseNotFound(res, {});
-            } else {
-                ResponseInternalServerError(res, {err: result.err});
-            }
-        });
-    }
-
     public kakaoLogin(req: Request, res: Response): void {
         let { id, nickname } = req.body;
 
@@ -29,23 +12,6 @@ class AuthController {
             username: id + nickname,
             time: new Date().getTime()
         })});
-    }
-    
-    public register(req: Request, res: Response): void {
-        let { username, password, realname, phoneNumber } = req.body;  
-        
-        user.register(username, password, realname, phoneNumber).then(result => {
-            ResponseSuccess(res, {token: encodeToken({
-                username: username,
-                time: new Date().getTime()
-            })});
-        }).catch(result => {
-            if (result.err == "the user already exist.") {
-                ResponseForbidden(res, {err: result.err});
-            } else {
-                ResponseInternalServerError(res, {err: result.err});
-            }
-        });
     }
 
     public getUserProfile(req: Request, res: Response): void {
--- inputs/auth.route.ts
+++ inputs/auth.route.ts
@@ -3,9 +3,7 @@ import authController from '../controllers/auth.controller'
 
 const router = Router();
 
-router.post('/login', authController.login);
 router.post('/kakaoLogin', authController.kakaoLogin)
-router.post('/register', authController.register);
 router.post('/getUserProfile', authController.getUserProfile);
 
 export default router;
